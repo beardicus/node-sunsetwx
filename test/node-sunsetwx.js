2d0e581ff105a01abd87ca9e26810a352228905a
Brian Boucheron <brian@boucheron.org>
2016-09-28 22:43:34 -0400

    add more tests

--- test/node-sunsetwx.js
+++ test/node-sunsetwx.js
@@ -2,10 +2,10 @@
 'use strict'
 
 var assert = require('assert')
-// var nock = require('nock')
+var nock = require('nock')
 var SunsetWx = require('../index.js')
 
-// var VERSION = require('../package.json').version
+var VERSION = require('../package.json').version
 
 describe('SunsetWx', function () {
   describe('Constructor', function () {
@@ -16,7 +16,9 @@ describe('SunsetWx', function () {
         email: null,
         password: null,
         key: null,
-        request_options: null
+        request_options: {
+          base_url: 'https://sunburst.sunsetwx.com/v1/'
+        }
       }
     })
 
@@ -68,6 +70,93 @@ describe('SunsetWx', function () {
 
       assert(client.options.hasOwnProperty('ping'))
       assert.equal(client.options.ping, options.ping)
+    })
+
+    it('has a default request object', function () {
+      var client = new SunsetWx(defaults)
+
+      assert(client.hasOwnProperty('request'))
+      assert(typeof client.request === 'function')
+    })
+
+    describe('Request Object', function () {
+      it('overrides default options') // TODO
+
+      it('accepts new options', function (done) {
+        var client = new SunsetWx({
+          request_options: {
+            headers: {foo: 'bar'}
+          }
+        })
+
+        nock('http://test').get('/').reply(200)
+
+        client.request('http://test/', function (err, response) {
+          assert.ifError(err)
+
+          var headers = response.request.headers
+
+          assert(headers.hasOwnProperty('foo'))
+          assert.equal(headers.foo, 'bar')
+
+          done()
+        })
+      })
+
+      it('expects json responses', function (done) {
+        var client = new SunsetWx()
+
+        nock('http://test').get('/').reply(200)
+
+        client.request('http://test/', function (err, response) {
+          assert.ifError(err)
+
+          var headers = response.request.headers
+
+          assert(headers.hasOwnProperty('accept'))
+          assert.equal(headers.accept, 'application/json')
+
+          done()
+        })
+      })
+
+      it('has the correct default User-Agent header', function (done) {
+        var client = new SunsetWx()
+
+        nock('http://test').get('/').reply(200)
+
+        client.request('http://test/', function (err, response) {
+          assert.ifError(err)
+
+          var headers = response.request.headers
+
+          assert(headers.hasOwnProperty('User-Agent'))
+          assert.equal(headers['User-Agent'], 'node-sunsetwx/' + VERSION)
+
+          done()
+        })
+      })
+
+      it('does not allow json settings to be changed', function (done) {
+        var client = new SunsetWx({
+          request_options: {
+            json: false
+          }
+        })
+
+        nock('http://test').get('/').reply(200)
+
+        client.request('http://test/', function (err, response) {
+          assert.ifError(err)
+
+          var headers = response.request.headers
+
+          assert(headers.hasOwnProperty('accept'))
+          assert.equal(headers.accept, 'application/json')
+
+          done()
+        })
+      })
     })
   })
 })
